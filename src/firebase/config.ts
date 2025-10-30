477f5fb7ab6bb26291c7939358ddb1c6bb5d060e
José Leonardo Lemos <covero.fbs@gmail.com>
2025-10-30 20:51:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-245966105-382a7",
+  "appId": "1:318032294296:web:ad428f5a5e58fcde913bed",
+  "apiKey": "AIzaSyD0vej0BIi3NXvTgVPbN0r__6HA0b3ZgeM",
+  "authDomain": "studio-245966105-382a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "318032294296"
+};
